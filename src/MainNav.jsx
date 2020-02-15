0a6e847884b1342d7be395661c10ec77723f9117
Paul Trampert <paul.trampert@ptrampert.com>
2020-02-15 17:41:17 -0500

    Add EOS logos.

--- src/MainNav.jsx
+++ src/MainNav.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Navbar from 'react-bootstrap/Navbar'
-import Button from 'react-bootstrap/Button'
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import {ReactComponent as EosIcon} from './EOS-icon.svg';
 
 function MainNav({onRequestBlocks, loadingBlocks}) {
   return (
     <Navbar bg="dark" variant="dark" className="justify-content-between">
-      <Navbar.Brand>EOS Block Viewer</Navbar.Brand>
+      <Navbar.Brand>
+        <EosIcon style={{marginRight: '0.5em'}} />
+        EOS Block Viewer
+      </Navbar.Brand>
       <Button onClick={onRequestBlocks} disabled={loadingBlocks}>Load</Button>
     </Navbar>
   );
